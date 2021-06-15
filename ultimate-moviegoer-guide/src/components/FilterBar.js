daa6d03c4df2455632dcf24eb6813f431eb694c0
Matthew Lloyd <mplloyd14@gmail.com>
2021-06-15 15:07:01 -0500

    Reset the query on filter change
    When a user clicks on a filter button after they have performed a query, I want to reset the query so that the filter data is displayed. I cleaned up the search onClick events.

--- ultimate-moviegoer-guide/src/components/FilterBar.js
+++ ultimate-moviegoer-guide/src/components/FilterBar.js
@@ -2,28 +2,19 @@
 This component will consist of different ways to filter the movie list. I.e. Popular, Now Playing, Top Rated.
 */
 
-const FilterBar = ({filter, setFilter, setPage}) => {
-	// I probably could have done inline functions within the button components to save line space.
-	const onClickNowPlaying = () => {
-		setFilter('now_playing');
-		setPage(1);
-	}
-
-	const onClickPopular = () => {
-		setFilter('popular')
-		setPage(1);
-	}
-
-	const onClickTopRated = () => {
-		setFilter('top_rated')
+const FilterBar = ({setFilter, setPage, setQuery}) => {
+	// We want to handle the onClick by resetting the query string, setting the appropriate filter, and resetting the page count.
+	const onClickFilter = (selectedFilter) => {
+		setQuery('');
+		setFilter(selectedFilter);
 		setPage(1);
 	}
 
 	return(
 		<div className='filterBar'>
-			<button className='nowPlaying' onClick={onClickNowPlaying}>Now Playing</button>
-			<button className='popular' onClick={onClickPopular}>Popular</button>
-			<button className='topRated' onClick={onClickTopRated}>Top Rated</button>
+			<button className='nowPlaying' onClick={() => onClickFilter('now_playing')}>Now Playing</button>
+			<button className='popular' onClick={() => onClickFilter('popular')}>Popular</button>
+			<button className='topRated' onClick={() => onClickFilter('top_rated')}>Top Rated</button>
 		</div>
 	)
 }
